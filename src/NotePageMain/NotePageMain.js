cd40c8f641551773f4526cccc239b5a1288374a3
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-16 12:37:59 -0700

    added server from repo

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -2,22 +2,32 @@ import React from 'react'
 import Note from '../Note/Note'
 import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
 import './NotePageMain.css'
+import NoteContext from '../noteContext'
 
-export default function NotePageMain(props) {
-  return (
+
+export default class NotePageMain extends React.Component{
+
+  static contextType = NoteContext;
+
+  render () {
+    const {noteId} = this.props.match.params;
+    const note = findNote(this.context.notes, noteId);
+
+    return (
     <section className='NotePageMain'>
       <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
+        id={note.id}
+        name={note.name}
+        modified={note.modified}
       />
       <div className='NotePageMain__content'>
-        {props.note.content.split(/\n \r|\n/).map((para, i) =>
+        {note.content.split(/\n \r|\n/).map((para, i) =>
           <p key={i}>{para}</p>
         )}
       </div>
     </section>
-  )
+    )
+}
 }
 
 NotePageMain.defaultProps = {
