1bbe5c595c709ed7ab02385468e941305fc1890f
Daniel Bright <brahyt@gmail.com>
2019-10-16 12:34:20 -0700

    pre merge

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -4,12 +4,19 @@ import {getNotesForFolder, findNote, findFolder} from '../notes-helpers';
 import './NotePageMain.css'
 
 export default function NotePageMain(props) {
+
+  render(){
+    const {noteId} = this.props.match.param
+    const note = findNote(this.context.notes, noteId )
+
+
+  }
   return (
     <section className='NotePageMain'>
       <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
+        id={note.id}
+        name={note.name}
+        modified={note.modified}
       />
       <div className='NotePageMain__content'>
         {props.note.content.split(/\n \r|\n/).map((para, i) =>
